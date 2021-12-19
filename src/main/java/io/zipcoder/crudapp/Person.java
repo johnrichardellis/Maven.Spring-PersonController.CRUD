aaa3e2d99746776c88d9e88da8a315092ce7dba7
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-12-19 12:59:05 -0500

    lab

---
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -0,0 +1,57 @@
+package io.zipcoder.crudapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String firstName;
+    private String lastName;
+
+    public Person(Long id, String firstName, String lastName) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Id=" + Id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,32 @@
+package io.zipcoder.crudapp;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class PersonController {
+
+
+    public Person createPerson(Person p) {
+
+    }
+
+    public Person getPerson(int id) {
+
+    }
+
+    public List<Person> getPersonList() {
+
+    }
+
+    public Person updatePerson(Person p) {
+
+    }
+
+    public void DeletePerson(int id) {
+
+    }
+
+
+}
